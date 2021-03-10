0f2aca78b9a54f31ca8f2e4d57899b04ad0e6659
ndirangu <ndirangu.muchai@gmail.com>
2021-03-10 20:19:04 +0300

    set up maps service

---
+++ maps-service/src/test/java/com/ndirangu/mapsservice/MapsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndirangu.mapsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
